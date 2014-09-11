b71c2c053ada89adf780b3e8d6cf8400a4956cc0
t94j0 <augeocacher@gmail.com>
2014-09-10 21:29:43 -0400

    Added some modules

--- src/com/maxwellharley/Fraction/Fraction.java
+++ src/com/maxwellharley/Fraction/Fraction.java
@@ -15,17 +15,33 @@ public class Fraction {
 	public int getDenominator() {
 		return denominator;
 	}
-	
-	public Fraction add(Fraction inp){
-		int gcd = gcd(inp.numerator, inp.denominator);
-		Fraction answer = new Fraction()
-	}
-	//My own
-	public int toDecimal(Fraction inp){
-		int answer = inp.numerator/inp.denominator;
+
+	public Fraction add(Fraction inp) {
+		int lcd = lcd(denominator, inp.getDenominator());
+		Fraction answer = new Fraction(1, 1);
+		if (numerator > inp.getNumerator()) {
+			answer = new Fraction((inp.getNumerator() * lcd) + (numerator), lcd);
+		} else if (numerator <= inp.getNumerator()) {
+			answer = new Fraction((inp.getNumerator()) + (numerator * lcd), lcd);
+		}
+		// answer = answer.reduce(answer);
 		return answer;
 	}
-	
+
+	public Fraction subtract(Fraction inp) {
+		int lcd = lcd(inp.getNumerator(), inp.getDenominator());
+		Fraction answer = new Fraction((inp.getNumerator() * lcd)
+				- (numerator * lcd), inp.getDenominator() * lcd);
+		answer = answer.reduce(answer);
+		return answer;
+	}
+
+	// My own
+	public int toDecimal(Fraction inp) {
+		int answer = inp.getNumerator() / inp.getDenominator();
+		return answer;
+	}
+
 	public Fraction multiply(Fraction inp) {
 		Fraction answer = new Fraction(numerator * inp.getNumerator(),
 				denominator * inp.getDenominator());
@@ -39,13 +55,15 @@ public class Fraction {
 		answer = reduce(answer);
 		return answer;
 	}
-	//my own
+
+	// my own
 	public Fraction recip(Fraction inp) {
-		Fraction answer = new Fraction(inp.denominator, inp.numerator);
+		Fraction answer = new Fraction(inp.getDenominator(), inp.getNumerator());
 		return answer;
 	}
-	//goes to the reduce function
-	public static int gcd(int a, int b) {
+
+	// goes to the reduce function
+	public int gcd(int a, int b) {
 		if (b == 0) {
 			return a;
 		} else {
@@ -53,10 +71,19 @@ public class Fraction {
 		}
 	}
 
+	public int lcd(int a, int b) {
+		for (int c = 1; c < b; c++) {
+			if (c * a % b == 0) {
+				return c * a;
+			}
+		}
+		return a * b;
+	}
+
 	public Fraction reduce(Fraction inp) {
-		int gcd = gcd(inp.numerator, inp.denominator);
-		Fraction answer = new Fraction(inp.numerator / gcd, inp.denominator
-				/ gcd);
+		int gcd = gcd(inp.getNumerator(), inp.getDenominator());
+		Fraction answer = new Fraction(inp.getNumerator() / gcd,
+				inp.getDenominator() / gcd);
 		return answer;
 	}
 
--- src/com/maxwellharley/Fraction/FractionDriver.java
+++ src/com/maxwellharley/Fraction/FractionDriver.java
@@ -1,12 +1,9 @@
 package com.maxwellharley.Fraction;
 
-import java.util.Random;
-
 public class FractionDriver {
 	public static void main(String[] args) {
-		Random r = new Random();
-		Fraction f = new Fraction(r.nextInt(9) + 1, r.nextInt(9) + 1);
-		Fraction f2 = new Fraction(8, 10);
+		Fraction f = new Fraction(1, 90);
+		Fraction f2 = new Fraction(1, 4);
 		System.out.println(f.multiply(f2));
 	}
 }
